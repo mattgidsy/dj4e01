841ca450c91575e587773200dd847f6b0327ab6e
inquiz <62821667+Inquiz@users.noreply.github.com>
2024-04-17 18:58:15 -0500

    fixed index problem

--- django_projects/mysite/polls/views.py
+++ django_projects/mysite/polls/views.py
@@ -6,6 +6,10 @@ from django.template import loader
 from .models import Question
 
 # Create your views here.
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
     try:
