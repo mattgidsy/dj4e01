de97df938c34558c432ec87d7a4edb17eeaac019
inquiz <62821667+Inquiz@users.noreply.github.com>
2024-04-17 17:31:30 -0500

    updated notes

--- django_projects/mysite/polls/views.py
+++ django_projects/mysite/polls/views.py
@@ -17,11 +17,11 @@ def owner(request):
    return HttpResponse("Hello, world. 5a77cde0 is the polls index.")
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s" % question_id)
+    return HttpResponse(f"You're looking at question {question_id}.")
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    response = f"You're looking at the results of question {question_id}."
+    return HttpResponse(response)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s.") % question_id
+    return HttpResponse(f"You're voting on question {question_id}.")
