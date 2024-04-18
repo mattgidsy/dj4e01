aefd409532865e4ec27335b32688c8936867e079
inquiz <62821667+Inquiz@users.noreply.github.com>
2024-04-17 20:27:05 -0500

    finished part3 of tutorial

--- django_projects/mysite/polls/views.py
+++ django_projects/mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from django.http import HttpResponse
 from django.template import loader
@@ -11,18 +11,12 @@ def index(request):
     context = {"latest_question_list": latest_question_list}
     return render(request, "polls/index.html", context)
 
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, "polls/detail.html", {"question": question})
-
 def owner(request):
    return HttpResponse("Hello, world. 5a77cde0 is the polls index.")
 
-# def detail(request, question_id):
-#     return HttpResponse(f"You're looking at question {question_id}.")
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
     response = f"You're looking at the results of question {question_id}."
